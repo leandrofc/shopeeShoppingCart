e0d13ae522117b0dfc77b5e105db1882e9b71ed3
Leandro Fonseca <leandromkt@outlook.com>
2025-06-24 23:27:49 -0300

    delete item

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ console.log("Welcome to the your Shopee cart!");
 const item1 = await createItem("Hotweels ferrari", 20.99, 1);
 const item2 = await createItem("Hotweels lamborguini", 39.99, 3);
 
+// adicionei 2 itens
 await cartService.addItem(myCart, item1);
-await cartService.addItem(myWishList, item2);
+await cartService.addItem(myCart, item2);
+
+// deletei 2 itens
+await cartService.deleteItem(myCart, item2.name);
+await cartService.deleteItem(myCart, item1.name);
 
 console.log("Shopee Cart total is:")
 await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -8,7 +8,11 @@ async function addItem(userCart, item){
 
 // -> deletar item
 async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item) => item.name === name);
 
+    if (index !== -1) {
+        userCart.splice(index, 1);
+    }
 }
 
 // -> remover um item
