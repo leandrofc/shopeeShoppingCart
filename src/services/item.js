67b5a9c9632711e1ac24d06801c8277f98aef83c
Leandro Fonseca <leandromkt@outlook.com>
2025-06-24 23:01:06 -0300

    implementando A lógica do item.js

--- src/services/item.js
+++ src/services/item.js
@@ -2,6 +2,12 @@
 
 // -> criar item com subtotal certo
 async function createItem(name, price, quantity){
-
+    return {
+        name,
+        price,
+        quantity,
+        subTotal: () => price * quantity,
+    }
 }
 
+export default createItem;
