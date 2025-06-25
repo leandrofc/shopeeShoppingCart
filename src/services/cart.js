556396e06fb63d6d07de568f973652d287ae4508
Leandro Fonseca <leandromkt@outlook.com>
2025-06-24 22:49:35 -0300

    Assinaturas Cart.js

---
+++ src/services/cart.js
@@ -0,0 +1,22 @@
+// Quais ações meu carrinho pode fazer
+
+//CASOS DE USO
+// -> adicionar item no carrinho
+async function addItem(userCart){
+
+}
+
+// -> deletar item
+async function deleteItem(userCart, name){
+
+}
+
+// -> remover um item
+async function removeItem(userCart, index){
+
+}
+
+// -> calcular o total do carrinho
+async function calculateTotal(userCart){
+    
+}
