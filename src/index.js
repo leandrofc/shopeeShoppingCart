eb17d11df6920f3a08659c40ad184a86eabb6676
Leandro Fonseca <leandromkt@outlook.com>
2025-06-24 23:19:16 -0300

    calculate total e wishlist

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 
 console.log("Welcome to the your Shopee cart!");
 
 const item1 = await createItem("Hotweels ferrari", 20.99, 1);
 const item2 = await createItem("Hotweels lamborguini", 39.99, 3);
 
-console.log(item2.subTotal());
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWishList, item2);
+
+console.log("Shopee Cart total is:")
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -18,7 +18,9 @@ async function removeItem(userCart, index){
 
 // -> calcular o total do carrinho
 async function calculateTotal(userCart){
-    return userCart.reduce((total, item) => total + item.subtotal(), 0);
+    const result = userCart.reduce((total, item) => total + item.subTotal(), 0);
+
+    console.log(result);
 }
 
 export { addItem, calculateTotal, deleteItem, removeItem };
